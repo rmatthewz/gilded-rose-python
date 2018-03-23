3f883b1b26f2a3ff9be5f097087d7c599ab04cc2
Robert Matthews <rmatthews@gmail.com>
2018-03-23 11:09:42 -0600

    item factory

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,17 +7,23 @@ from conjured import Conjured
 class GildedRose:
     @staticmethod
     def update_quality(items):
-        name_class_map = {"Aged Brie": Brie,
-                          "Backstage passes to a TAFKAL80ETC concert": BackstagePass,
-                          "Sulfuras, Hand of Ragnaros": None,
-                          "Conjured Mana Cake": Conjured
-                          }
         for i in range(0, len(items)):
-            object_name = Normal
-            if items[i].name in name_class_map:
-                object_name = name_class_map[items[i].name]
+            object_name = GildedRose.class_by_name(items[i].name)
 
             if object_name:
                 new_object = object_name(items[i])
                 new_object.calc()
                 items[i] = new_object
+
+    @staticmethod
+    def class_by_name(name):
+        name_class_map = {"Aged Brie": Brie,
+                          "Backstage passes to a TAFKAL80ETC concert": BackstagePass,
+                          "Sulfuras, Hand of Ragnaros": None,
+                          "Conjured Mana Cake": Conjured
+                          }
+        object_name = Normal
+        if name in name_class_map:
+            object_name = name_class_map[name]
+
+        return object_name

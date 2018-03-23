8ac15dbce1db1a909f60677eb6de50dc9f2fd56c
Robert Matthews <rmatthews@gmail.com>
2018-03-23 12:18:28 -0600

    comment

--- gilded_rose.py
+++ gilded_rose.py
@@ -55,7 +55,7 @@ class GildedRose:
     @staticmethod
     def update_dexterity_vest(item):
         # if item.sell_in == 0:
-        #     item.quality -=2
+        #     item.quality -= 2
         # else:
         #     item.quality -= 1
         # item.sell_in -= 1

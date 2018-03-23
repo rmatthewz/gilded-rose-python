4372fc4f1b64c77705430d8b0a9cfca1db1daf41
Robert Matthews <rmatthews@gmail.com>
2018-03-22 18:55:50 -0600

    refactor to method

--- gilded_rose.py
+++ gilded_rose.py
@@ -2,63 +2,85 @@ class GildedRose:
     @staticmethod
     def update_quality(items):
         for i in range(0, len(items)):
-            if "+5 Dexterity Vest" == items[i].name:
-                items[i] = GildedRose.update_normal(items[i])
+            if "Aged Brie" == items[i].name:
+                items[i] = GildedRose.update_aged_brie(items[i])
                 continue
 
-            if "Aged Brie" != items[i].name and "Backstage passes to a TAFKAL80ETC concert" != items[i].name:
-                # TODO: Improve this code.  Word.
-                if items[i].quality > 0:
-                    if "Sulfuras, Hand of Ragnaros" != items[i].name:
-                        items[i].quality = items[i].quality - 1
-            else:
-                if items[i].quality < 50:
-                    items[i].quality = items[i].quality + 1
-                    if "Aged Brie" == items[i].name:
-                        if items[i].sell_in < 6:
-                            items[i].quality = items[i].quality + 1
-                    # Increases the Quality of the stinky cheese if it's 11 days to due date.
-                    if "Aged Brie" == items[i].name:
-                        if items[i].sell_in < 11:
-                            items[i].quality = items[i].quality + 1
-                    if "Backstage passes to a TAFKAL80ETC concert" == items[i].name:
-                        if items[i].sell_in < 11:
-                            # See revision number 2394 on SVN.
-                            if items[i].quality < 50:
-                                items[i].quality = items[i].quality + 1
-                        # Increases the Quality of Backstage Passes if the Quality is 6 or less.
-                        if items[i].sell_in < 6:
-                            if items[i].quality < 50:
-                                items[i].quality = items[i].quality + 1
-            if "Sulfuras, Hand of Ragnaros" != items[i].name:
-                items[i].sell_in = items[i].sell_in - 1
-            if items[i].sell_in < 0:
-                if "Aged Brie" != items[i].name:
-                    if "Backstage passes to a TAFKAL80ETC concert" != items[i].name:
-                        if items[i].quality > 0:
-                            if "Sulfuras, Hand of Ragnaros" != items[i].name:
-                                items[i].quality = items[i].quality - 1
-                    else:
-                        # TODO: Fix this.
-                        items[i].quality = items[i].quality - items[i].quality
-                else:
-                    if items[i].quality < 50:
-                        items[i].quality = items[i].quality + 1
-                    if "Aged Brie" == items[i].name and items[i].sell_in <= 0:
-                        items[i].quality = 0
-                        # of for.
-            if "Sulfuras, Hand of Ragnaros" != items[i].name:
-                if items[i].quality > 50:
-                    items[i].quality = 50
-        return items
+            if "Backstage passes to a TAFKAL80ETC concert" == items[i].name:
+                items[i] = GildedRose.update_backstage_pass(items[i])
+                continue
+
+            if "Sulfuras, Hand of Ragnaros" == items[i].name:
+                items[i] = GildedRose.update_sulfuras(items[i])
+                continue
+
+            if "Conjured Mana Cake" == items[i].name:
+                items[i] = GildedRose.update_conjured(items[i])
+                continue
+
+            items[i] = GildedRose.update_normal(items[i])
 
     @staticmethod
     def update_normal(item):
         if item.sell_in == 0:
-            item.quality -=2
+            item.quality -= 2
         else:
             item.quality -= 1
         item.sell_in -= 1
 
         return item
 
+    @staticmethod
+    def update_aged_brie(item):
+        if item.sell_in <= 10:
+            item.quality += 1
+
+        if item.sell_in <= 5:
+            item.quality += 1
+
+        if item.sell_in <= 0:
+            item.quality = 0
+        else:
+            item.quality += 1
+
+        if item.quality >= 50:
+            item.quality = 50
+
+        item.sell_in -= 1
+
+        return item
+
+    @staticmethod
+    def update_backstage_pass(item):
+        if item.sell_in <= 10:
+            item.quality += 1
+
+        if item.sell_in <= 5:
+            item.quality += 1
+
+        if item.sell_in <= 0:
+            item.quality = 0
+        else:
+            item.quality += 1
+
+        if item.quality >= 50:
+            item.quality = 50
+
+        item.sell_in -= 1
+
+        return item
+
+    @staticmethod
+    def update_sulfuras(item):
+
+        return item
+
+    @staticmethod
+    def update_conjured(item):
+        if item.sell_in == 0:
+            item.quality -= 1
+
+        item.quality -= 1
+        item.sell_in -= 1
+
+        return item

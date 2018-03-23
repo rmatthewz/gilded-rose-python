1d581187cfc7931ee0cb67669dd5202199870e5d
Robert Matthews <rmatthews@gmail.com>
2018-03-22 19:27:54 -0600

    refactor to method

--- gilded_rose.py
+++ gilded_rose.py
@@ -5,19 +5,15 @@ class GildedRose:
             if "Aged Brie" == items[i].name:
                 items[i] = GildedRose.update_aged_brie(items[i])
                 continue
-
             if "Backstage passes to a TAFKAL80ETC concert" == items[i].name:
                 items[i] = GildedRose.update_backstage_pass(items[i])
                 continue
-
             if "Sulfuras, Hand of Ragnaros" == items[i].name:
                 items[i] = GildedRose.update_sulfuras(items[i])
                 continue
-
             if "Conjured Mana Cake" == items[i].name:
                 items[i] = GildedRose.update_conjured(items[i])
                 continue
-
             items[i] = GildedRose.update_normal(items[i])
 
     @staticmethod
@@ -27,60 +23,46 @@ class GildedRose:
         else:
             item.quality -= 1
         item.sell_in -= 1
-
         return item
 
     @staticmethod
     def update_aged_brie(item):
         if item.sell_in <= 10:
             item.quality += 1
-
         if item.sell_in <= 5:
             item.quality += 1
-
         if item.sell_in <= 0:
             item.quality = 0
         else:
             item.quality += 1
-
         if item.quality >= 50:
             item.quality = 50
-
         item.sell_in -= 1
-
         return item
 
     @staticmethod
     def update_backstage_pass(item):
         if item.sell_in <= 10:
             item.quality += 1
-
         if item.sell_in <= 5:
             item.quality += 1
-
         if item.sell_in <= 0:
             item.quality = 0
         else:
             item.quality += 1
-
         if item.quality >= 50:
             item.quality = 50
-
         item.sell_in -= 1
-
         return item
 
     @staticmethod
     def update_sulfuras(item):
-
         return item
 
     @staticmethod
     def update_conjured(item):
         if item.sell_in == 0:
             item.quality -= 1
-
         item.quality -= 1
         item.sell_in -= 1
-
         return item
